20f538dcdfc0f286bb845df7978bb213dbc4b63f
Vikram <vikram1vicky@gmail.com>
2017-03-17 15:08:56 +0530

    Changed layout

--- js/app.js
+++ js/app.js
@@ -1,96 +1,137 @@
 (((R, RD) => {
 	
-const ce = R.createElement,
+const 	
+	ce = R.createElement,
+	RC = React.Component,
 		
-		/* Helper methods to various sections (DOM partials) of App
-		 * just to keep the code clearner 
-		 */
-		_renderHeader = () => {
-			return ce('h1', null, 'Hisaab Kitab');
-		},
-		_renderWizardNav = () => {
-			return ce('nav', { id: 'wizardSteps' },
-						ce('ul', null,
-							ce('li', null, ce('a', { className: 'active' }, 'Buddies')),
-							ce('li', null, ce('a', null, 'Spendings')),
-							ce('li', null, ce('a', null, 'Spenders'))
-						)
-						
-					);
-		},
-		_renderTextInput = (placeholder1, placeholder2) => {
+	_renderRows = (data, onRemove) => {
 			
-			let = clas1 = placeholder2 ? 'has-amount' : '';
-			
-			return ce('div', { className: 'inputs-row' },
-						ce('input', { placeholder: placeholder1, type: 'text', className:  clas1 }),
-						(placeholder2 && ce('input', { placeholder: placeholder2, type: 'text', className: 'amount' }))
-					);
-		},
-		_renderForm = (id, title, btnId, addBtnId, placeholder1, placeholder2) => {
-			
-			return ce('form', { id: id, name: id },
-						ce('h2', null, title,
-							ce('div', { className: 'btns-wrapper' },
-								ce('a', { className: 'btn-primary', id: addBtnId }, 'Add Row'),
-								ce('a', { className: 'btn-primary', id: btnId }, 'Next')
-							)
-						),
-						ce('fieldset', null,
-							_renderTextInput(placeholder1, placeholder2),
-							_renderTextInput(placeholder1, placeholder2),
-							_renderTextInput(placeholder1, placeholder2)
-						)
-						
-					);
-		},
-		_renderBuddiesForm = () => {
-			
-			return _renderForm('formBuddies', 'Type in Buddies name', 'btnBuddies', 'btnNewBuddy', 'Buddy name');
-		},
-		_renderSpendingsForm = () => {
-			
-			return _renderForm('formSpendings', 'Type in Spendings', 'btnSpendings', 'btnNewSpending', 'Spending', 'Amount');
-		},
-		_renderSpendersForm = () => {
-			
-			return _renderForm('formSpenders', 'Type in Spenders name', 'btnSpenders', 'btnNewSpender', 'Spender', 'Amount');
+			return data.map((item, indx) => {
+				item.key = item.id = indx;
+				item.onRemove = onRemove;
+				return ce(FormRow, item);
+			});
 		}
 ;
-	
-	class App extends React.Component {
+
+class FormRow extends RC {
 		
-		constructor(props) {
+	constructor(props) {
 		
-	    	super(props);
-	    	
-	    	this.state = {
-	    		spendings: [],
-	    		buddies: [],
-	    		spenders: []
-	    	}
-			
-		}
+		super(props);
 		
-		render() {
-			
-			return (
-				// Main Wrapper
-				ce('div', { id: 'appWrapper' },
-					_renderHeader(),
-					_renderWizardNav(),
-					ce('div', {id: 'contentSection'},
-						_renderBuddiesForm(),
-						_renderSpendingsForm(),
-						_renderSpendersForm()
-					)
-				)
-			);
-		}
+		this.remove = this.remove.bind(this);
 	}
 	
-	console.log(new App());
+	remove(evt) {
+		
+		evt.preventDefault();
+		evt.stopPropagation();
+		this.props.onRemove(this.props.id);
+	}
 	
-	RD.render(ce(App), document.getElementById('root'));
+	render() {
+		
+		return ce('fieldset', { className: 'inputs-row', ref: fieldset => {this.node = fieldset} },
+			(this.props.removable && ce('input', {type: 'button', value: 'X', className: 'btn-general', onClick: this.remove})),
+			ce('input', { 
+				placeholder: 'Spent by', 
+				type: 'text',
+				required: true,
+				title: 'Who spent?',
+				value: this.props.spentBy }),
+			ce('input', { 
+				placeholder: 'Spent for', 
+				type: 'text',
+				required: true,
+				title: 'Enter expenses done for?',
+				value: this.props.spentOn }),
+			ce('input', { 
+				placeholder: 'Amount', 
+				type: 'text',
+				min: 0,
+				max: 50000,
+				pattern: '^\\d{1,5}(\\.\\d{1,2})?$', 
+				required: true,
+				title: 'How much was spent?',
+				value: this.props.amount })
+		);
+	}
+}
+
+class App extends RC {
+	
+	constructor(props) {
+	
+    	super(props);
+    	
+    	this.state = {
+    		data: [
+    			{ spentBy: '', spentOn: '', amount: 0.00, removable: false }
+				, { spentBy: '', spentOn: '', amount: 0.00, removable: false }
+				, { spentBy: '', spentOn: '', amount: 0.00, removable: false }
+				, { spentBy: '', spentOn: '', amount: 0.00, removable: true }
+				, { spentBy: '', spentOn: '', amount: 0.00, removable: true }
+    		]
+    	};
+    	
+    	this.calc = this.calc.bind(this);
+    	this.addRow = this.addRow.bind(this);
+    	this.delRow = this.delRow.bind(this);
+	}
+	
+	addRow(evt) {
+		
+		evt.preventDefault();
+		evt.stopPropagation();
+		
+		this.setState({
+			data: this.state.data.concat({ id: this.state.data.length, spentBy: '', spentOn: '', amount: 0.00, removable: true })
+		});
+	}
+	
+	delRow(id) {
+		
+		this.state.data.splice(id, 1);
+		
+		this.setState({
+			data: this.state.data
+		});
+	}
+	
+	calc(evt) {
+		
+		//evt.preventDefault();
+		//evt.stopPropagation();
+	}
+	
+	render() {
+		
+		return (
+			// Main Wrapper
+			ce('div', { id: 'appWrapper' },
+				ce('h1', null, 'Hisaab Kitab'),
+				ce('div', {id: 'contentSection'},
+					ce('h2', {}, 'Type in expenses details...'),
+					ce('form', {name: 'mainForm', id: 'mainForm', onSubmit: this.calc},
+						ce('div', { className: 'fields-wrapper'},
+							ce('div', { className: 'fields-wrapper-inner'},
+								_renderRows(this.state.data, this.delRow)
+							)
+						),
+						ce('div', {id: 'bottomBar'},
+							ce('input', {type: 'button', id: 'btnNewRow', value: '+', className: 'btn-general', onClick: this.addRow}),
+							ce('input', {type: 'submit', id: 'btnCalc', value: 'Calculate', className: 'btn-primary'})
+						)
+					)
+				)
+			)
+		);
+	}
+}
+
+console.log(new App());
+
+RD.render(ce(App), document.getElementById('root'));
 	
 })(React, ReactDOM));
